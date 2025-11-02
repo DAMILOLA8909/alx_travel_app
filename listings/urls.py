553455e6bd37248f71b99792e7355104228d26a0
DAMILOLA8909 <damsony.soji@gmail.com>
2025-11-02 18:51:15 +0100

    Initial project setup with Django, REST framework, MySQL, and Swagger documentation

--- listings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'listings'
-
-urlpatterns = [
-    # We'll add actual endpoints later
-    path('', views.api_overview, name='api-overview'),
-]
